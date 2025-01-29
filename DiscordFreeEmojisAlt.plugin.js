10a5b052c8bdf4617685ad79adf9b051153dd558
EpicGazel <zaneshark@gmail.com>
2025-01-28 21:14:41 -0500

    Version bump Alt

--- DiscordFreeEmojisAlt.plugin.js
+++ DiscordFreeEmojisAlt.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name FreeEmojis
- * @version 1.8.5.2
+ * @version 1.8.5.3
  * @description Link emojis if you don't have nitro! Type them out or use the emoji picker!
  * @author An0 (Original) & EpicGazel 
  * @source https://github.com/EpicGazel/DiscordFreeEmojis
